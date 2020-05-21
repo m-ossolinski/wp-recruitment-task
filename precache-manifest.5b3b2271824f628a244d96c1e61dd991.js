9b43da2906de17e506bbf9f53f915ad6ce8c1205
m-ossolinski <mateusz.ossolinski@gmail.com>
2020-05-21 21:52:08 +0200

    Updates

---
+++ precache-manifest.5b3b2271824f628a244d96c1e61dd991.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f45eb1560fa5d40038a4a2b7f93fa8b",
+    "url": "/wp-recruitment-task/index.html"
+  },
+  {
+    "revision": "470493a53e33893171c6",
+    "url": "/wp-recruitment-task/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "239bcaf7327f7d6589ff",
+    "url": "/wp-recruitment-task/static/js/2.fde52ffe.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/wp-recruitment-task/static/js/2.fde52ffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "470493a53e33893171c6",
+    "url": "/wp-recruitment-task/static/js/main.8e7e76d1.chunk.js"
+  },
+  {
+    "revision": "c6fc93e1ada980929dca",
+    "url": "/wp-recruitment-task/static/js/runtime-main.f0899697.js"
+  },
+  {
+    "revision": "9f6dd2df83e47f8fe6cbd86ae98af96b",
+    "url": "/wp-recruitment-task/static/media/cartIcon.9f6dd2df.svg"
+  },
+  {
+    "revision": "c53ebe467e7a1cdd3a8488d365e0d355",
+    "url": "/wp-recruitment-task/static/media/sw-bg.c53ebe46.jpg"
+  }
+]);
